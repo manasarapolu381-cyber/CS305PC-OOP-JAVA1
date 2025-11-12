d01f1dfe76706589a445df1c8c835246204191cf
manasarapolu381-cyber <manasarapolu381@gmail.com>
2025-11-12 12:26:36 +0530

    Add files via upload

---
+++ LAB2/Lab2p2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Lab2p2{
+	public static void main(String...args){
+		Scanner sc=new Scanner(System.in);
+		int x=s.nextInt();
+		int y=x;
+		System.out.println("output is:"+ k++ + ++k);
+		x=y;
+		System.out.println("output is:"+ (k++ + ++k));
+		x=y;
+		System.out.println(k++ + ++k +": output is");
+	}
+}		
